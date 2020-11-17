7d5be1c2f5fdfc139c5697ce59c101be726f77b2
Hugo Teixeira <hugo.teixeira1103@gmail.com>
2020-11-17 20:05:24 -0300

    Atualizando projeto

---
+++ Assets/TextMesh Pro/Scripts/PanelRankingController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PanelRankingController : MonoBehaviour
+{
+    public RectTransform painelColocacoes;
+    public TextMeshProUGUI textoSemResultados;
+    public TextMeshProUGUI[] textPontuacao = new TextMeshProUGUI[5];
+    public GameObject[] textColocacao = new GameObject[5];
+
+    public List<int> lista = new List<int>();
+    public bool isReady;
+
+    void Start()
+    {
+        ControlePaineis(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<RankingController>().GetLista != null)
+        {
+            lista = GetComponent<RankingController>().GetLista;
+
+            if (lista.Count > 0)
+            {
+                ControlePaineis(true);
+            }
+        }
+
+        if (painelColocacoes.gameObject.active && lista != null)
+        {
+            for (int i = 0; i < textPontuacao.Length; i++)
+            {
+                if (i < lista.Count)
+                {
+                    textPontuacao[i].SetText(lista[i].ToString());
+                }
+                else
+                {
+                    textColocacao[i].SetActive(false);
+                }
+            }
+        }
+    }
+
+    private void ControlePaineis(bool input)
+    {
+        painelColocacoes.gameObject.SetActive(input);
+        textoSemResultados.gameObject.SetActive(!input);
+    }
+}
---
+++ Assets/TextMesh Pro/Scripts/RankingController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class RankingController : MonoBehaviour
+{
+    private static List<int> lista = new List<int>();
+    private static string path = ".\\Assets\\Configure\\rank.txt";
+    private static StreamWriter texto;
+    private string[] leitura;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leitura = File.ReadAllLines(path);
+
+        if (leitura.Length > 0)
+        {
+            LeituraRank(path);
+        }
+    }
+
+    public void LeituraRank(string path)
+    {
+        int[] numeros = new int[leitura.Length];
+        Debug.Log("Textos lidos: " + leitura.Length);
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            numeros[i] = int.Parse(leitura[i]);
+        }
+
+        // Ordena a lista para decrescente
+        List<int> listaDecrescente = numeros.OrderByDescending(i => i).ToList<int>();
+        
+        lista = listaDecrescente;
+        Debug.Log("Lista quantidade: " + lista.Count);
+    }
+
+    public static void CriarArquivo(int pontuacaoAdquirida)
+    {
+        if (!File.Exists(path))
+        {
+            texto = File.CreateText(path);
+            texto.Close();
+            EscritaRank(pontuacaoAdquirida);
+        }
+        else
+        {
+            EscritaRank(pontuacaoAdquirida);
+        }
+        
+    }
+
+    public static void EscritaRank(int pontuacaoAdquirida)
+    {
+        texto = new StreamWriter(path, true);
+
+        lista.Add(pontuacaoAdquirida);
+
+        for (int i = 0; i < lista.Count; i++)
+        {
+            texto.WriteLine(path, File.ReadAllLines(path)[i]);
+        }
+        texto.Close();
+
+
+        foreach (int i in lista)
+        {
+            Debug.Log("VALOR DO RANK: " + i);
+        }
+        Debug.Log("TAMANHO DE N NA LISTA: " + lista.Count);
+    }
+
+    public List<int> GetLista
+    {
+        get
+        {
+            return lista;
+        }
+    }
+
+}
